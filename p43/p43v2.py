9a71609eea2726ff8a95da7aa17ac19b1de3d1db
Tony Majestro <tmajest@microsoft.com>
2013-12-04 19:47:33 -0800

    Changed v2 to use xrange

--- p43/p43v2.py
+++ p43/p43v2.py
@@ -8,7 +8,7 @@ import itertools
 
 def has_weird_property(num_str):
     divisors = [2, 3, 5, 7, 11, 13, 17]
-    return all(int(num_str[i+1:i+4]) % divisors[i] == 0 for i in range(7))
+    return all(int(num_str[i+1:i+4]) % divisors[i] == 0 for i in xrange(7))
 
 pandigitals = [''.join(p) for p in itertools.permutations('0123456789')]
 print sum(int(p) for p in pandigitals if has_weird_property(p))
