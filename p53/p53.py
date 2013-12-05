76ef07b50646ca7ed6e332ca7838f1a4c66d4451
Tony Majestro <tmajest@microsoft.com>
2013-12-04 18:39:18 -0800

    Changed to use xrange, uses less memory

--- p53/p53.py
+++ p53/p53.py
@@ -3,9 +3,9 @@
 import operator
 
 def factorial(n):
-    return 1 if n == 0 else reduce(operator.mul, range(1, n+1), 1)
+    return 1 if n == 0 else reduce(operator.mul, xrange(1, n+1), 1)
 
 def choose(n, r):
     return factorial(n) / (factorial(r) * factorial(n - r))
 
-print len([1 for n in range(1, 101) for r in range(n) if choose(n, r) > 1000000])
+print len([1 for n in xrange(1, 101) for r in xrange(n) if choose(n, r) > 1000000])
