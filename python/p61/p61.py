2bf71b7b60c3bf1b725da010578e534a24bef803
Tony Majestro <tmajest@vt.edu>
2013-12-09 21:16:58 -0800

    added p61

--- python/p61/p61.py
+++ python/p61/p61.py
@@ -1,5 +1,25 @@
 # http://projecteuler.net/problem=61
 
+def solve(curr_sequence, all_num_lists, used):
+    if len(used) == 6 and curr_sequence[-1][-2:] == curr_sequence[0][:2]:
+        return sum(map(int, curr_sequence))
+
+    for pos, num_list in enumerate(all_num_lists):
+        if pos in used:
+            continue
+
+        used.add(pos)
+        for num in num_list:
+            if len(curr_sequence) == 0 or curr_sequence[-1][-2:] == num[:2]:
+                curr_sequence.append(num)
+                ret = solve(curr_sequence, all_num_lists, used)
+                if ret:
+                    return ret
+                curr_sequence.pop()
+
+        used.remove(pos)
+
+    
 P = { 3: lambda n: n * (n + 1) / 2,
       4: lambda n: n * n,
       5: lambda n: n * (3 * n - 1) / 2,
@@ -7,10 +27,14 @@ P = { 3: lambda n: n * (n + 1) / 2,
       7: lambda n: n * (5 * n - 3) / 2,
       8: lambda n: n * (3 * n - 2) }
 
+triangle_nums   = [ str(P[3](n)) for n in range(1000) if 1000 <= P[3](n) <= 9999 ]
+square_nums     = [ str(P[4](n)) for n in range(1000) if 1000 <= P[4](n) <= 9999 ]
+pentagonal_nums = [ str(P[5](n)) for n in range(1000) if 1000 <= P[5](n) <= 9999 ]
+hexagonal_nums  = [ str(P[6](n)) for n in range(1000) if 1000 <= P[6](n) <= 9999 ]
+heptagonal_nums = [ str(P[7](n)) for n in range(1000) if 1000 <= P[7](n) <= 9999 ]
+octagonal_nums  = [ str(P[8](n)) for n in range(1000) if 1000 <= P[8](n) <= 9999 ]
 
-triangle_nums   = { P[3](n) for n in range(1000) if 1000 <= P[3](n) <= 9999 }
-square_nums     = { P[4](n) for n in range(1000) if 1000 <= P[4](n) <= 9999 }
-pentagonal_nums = { P[5](n) for n in range(1000) if 1000 <= P[5](n) <= 9999 }
-hexagonal_nums  = { P[6](n) for n in range(1000) if 1000 <= P[6](n) <= 9999 }
-heptagonal_nums = { P[7](n) for n in range(1000) if 1000 <= P[7](n) <= 9999 }
-octagonal_nums  = { P[8](n) for n in range(1000) if 1000 <= P[8](n) <= 9999 }
+all_nums = [triangle_nums, square_nums, pentagonal_nums, 
+            hexagonal_nums, heptagonal_nums, octagonal_nums]     
+
+print solve([], all_nums, set())
