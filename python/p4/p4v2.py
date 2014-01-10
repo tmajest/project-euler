43c4c1eed895c3df680e9d1051a8439caaef933f
Tony Majestro <tmajest@microsoft.com>
2014-01-09 17:51:09 -0800

    Changed p4v2

--- python/p4/p4v2.py
+++ python/p4/p4v2.py
@@ -5,6 +5,9 @@
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-print max(i * j for i in range(100, 1000) 
-                    for j in range(100, 1000)
-                        if str(i * j) == str(i * j)[::-1])
+def is_palindrome(num):
+    num_str = str(num)
+    return num_str == num_str[::-1]
+
+products = [i * j for i in range(100, 1000) for j in range(100, 1000)]
+print max(product for product in products if is_palindrome(product))
